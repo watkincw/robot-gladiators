77772a87ba8cc3e64673ab186205d236cf93680e
Colton <watkincw@gmail.com>
2021-06-04 17:46:47 -0500

    defined variables of characters, added fight function, added health checks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,40 @@
+window.alert("Welcome to Robot Gladiators!");
 var playerName = window.prompt("What is your robot's name?");
-// window.alert(playerName); // stores value of above var playerName in the browser
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() { 
-    window.alert("The fight has begun!");
-}
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+var fight = function(){
+    window.alert("The battle has started!");
+
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // checking enemy's health:
+    if (enemyHealth <=0) { 
+        window.alert(enemyName + " has died!");
+    }
+    else { 
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    )
+    if (playerHealth <=0) { 
+        window.alert(playerName + " has died!");
+    }
+    else { 
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+fight();
