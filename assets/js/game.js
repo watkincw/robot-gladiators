a2baed1ef140d2ad226bd9e0ab750b7a2ae65d53
Colton <watkincw@gmail.com>
2021-06-08 16:28:33 -0500

    start and endgame functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,28 +69,75 @@ var fight = function(enemyName) {
     }
 };
 
-// fight each enemy-robot looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) { 
-    // if player is still alive, keep fighting
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    var playerHealth = 100;
+    var playerAttack = 10;
+    var playerMoney = 10;
+    // fight each enemy-robot looping over them and fighting them one at a time
+    for (var i = 0; i < enemyNames.length; i++) { 
+        // if player is still alive, keep fighting
+        if (playerHealth > 0) { 
+            // let player know what round they are in, remember that arrays start at 0 so it need to have 1 added to it
+            window.alert('Welsome to Robot Gladiators! Round ' + (i + 1));
+
+            // pick new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
+
+            // reset enemyHealth before starting new fight
+            enemyHealth = 50;
+
+            // use debugger to pause script from running and check what's going on at that moment in
+            // the code debugger;
+
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+        }
+        // if player isnt alive, stop the game
+        else { 
+            window.alert('You have lost your robot in battle! Game Over!');
+            break;
+        }
+    }
+    // after the loop ends, the player is either out of health or eneimies to fight, so run the endGame function
+    endGame();
+};
+
+// funstion to end the entire game
+var endGame = function() { 
+    window.alert("The game has now ended. Let's see how you did!");
+    // if player is still alive, player wins!
     if (playerHealth > 0) { 
-        // let player know what round they are in, remember that arrays start at 0 so it need to have 1 added to it
-        window.alert('Welsome to Robot Gladiators! Round ' + (i + 1));
-
-        // pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
-
-        // reset enemyHealth before starting new fight
-        enemyHealth = 50;
-
-        // use debugger to pause script from running and check what's going on at that moment in
-        // the code debugger;
-
-        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
-    // if player isnt alive, stop the game
     else { 
-        window.alert('You have lost your robot in battle! Game Over!');
-        break;
+        window.alert("You've lost your robot in battle.");
     }
-}
+    // ask the player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) { 
+        // restart the game
+        startGame();
+    }
+    else { 
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+startGame();
+
+// Wrap the game in logic in a startGame() function
+// When the player is defeated or there are no more enemies, call an endGame() function that:
+    // Alerts the player's total stats
+    // Asks the player if they want to play again
+    // If yes, call startGame() to restart the game
+// After the player skips or ddefeats an enemy(and there are still more robots to fight):
+    // Ask the player if they want to "shop"
+    // If no, continue as normal
+    // If yes, call the shop() function
+    // In the shop() function, ask the player if they want to "refill" health, "upgrade", attack, or "leave" the shop
+    // If refill, subtract money from player and increse health
+    // If upgrade, subtract money from player and increase attack power
+    // If leave, alert goobye and exit the function
+    // If any other invalid option, call shop() again
